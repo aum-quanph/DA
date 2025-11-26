739d3d6a103262c15fbf5eb9977c41e36d878ef3
Thienquan <nonolive2312@gmail.com>
2025-11-26 10:32:58 +0700

    update change

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent, title: 'App Home'},
+    {path: 'login', component: LoginComponent, title: 'App Login'},
+    {path: 'register', component: RegisterComponent, title: 'App Register'},
+];
