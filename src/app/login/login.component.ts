d87003e781b0506d03132967dad248b526a8cbce
Thienquan <nonolive2312@gmail.com>
2025-11-26 11:06:54 +0700

    update form login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  LoginForm = new FormGroup({
+    userName: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
+  onSubmit() {
+    if(this.LoginForm.valid) {
+      const formData = this.LoginForm.value;
+      const userName = formData.userName;
+      const password = formData.password;
+
+      alert('Đăng nhập thành công: \n' + 'User Name: ' + userName + '\n' + 'Password: ' + password);
+    }
+
+  }
 }
