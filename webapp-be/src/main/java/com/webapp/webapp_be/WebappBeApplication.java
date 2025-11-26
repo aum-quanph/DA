29e8a9d3a206239ace8555763add5fc6288f79df
Thienquan <nonolive2312@gmail.com>
2025-11-26 11:38:50 +0700

    add web app backend

---
+++ webapp-be/src/main/java/com/webapp/webapp_be/WebappBeApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.webapp_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappBeApplication.class, args);
+	}
+
+}
---
+++ webapp-be/src/test/java/com/webapp/webapp_be/WebappBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webapp.webapp_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
